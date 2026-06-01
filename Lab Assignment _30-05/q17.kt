ed65a0d145c56a6878e87bc7f323fff01d6e3575
Hitesh Kumar Rana <ranahitesh2006@gmail.com>
2026-06-01 08:23:43 +0530

    Add files via upload

---
+++ Lab Assignment _30-05/q17.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val input: String? = "Hitesh"
+
+    input?.let {
+        println("Length = ${it.length}")
+    }
+}
---
+++ Lab Assignment _30-05/q18.kt
@@ -0,0 +1,7 @@
+suspend fun fetchWeather(): String {
+    return "Sunny"
+}
+
+fun main() {
+    println("Weather: Sunny")
+}
